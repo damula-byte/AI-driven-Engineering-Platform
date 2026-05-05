c9c811dcc030cc9a097c6eb168fdbb6fbbc31771
Nguyễn Lê Xuân Thịnh <xuanthinhnguyenle0701@gmail.com>
2026-05-05 22:03:35 +0700

    newww

--- Translator_CLI/TIA Portal V20/TIA_V20.cs
+++ Translator_CLI/TIA Portal V20/TIA_V20.cs
@@ -493,7 +493,7 @@ namespace Middleware_console
             string obDefaultName = "OB" + obNumber;
 
             // 1.2. Quét giá trị CyclicTime từ Comment
-            int dynamicCyclicTime = 100000; // Mặc định 100000us nếu không tìm thấy
+            int dynamicCyclicTime = 500; // Mặc định 500us đối với s71500 và 500ms đối với s71200 nếu không tìm thấy
             var timeMatch = Regex.Match(sclContent, @"@CyclicTime:\s*(\d+)us", RegexOptions.IgnoreCase);
             if (timeMatch.Success)
             {
